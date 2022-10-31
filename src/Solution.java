6e01d3730aafc7cfc1f39d0378cdda15749157cd
memo <alexis45645@gmail.com>
2022-10-30 22:21:34 -0400

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,10 @@
+
+public class Solution {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(divide(-2147483647,-1));
+	}
+	public static int divide(int dividend, int divisor) {
+        return (int)((double)dividend/(double)divisor);
+    }
+}
